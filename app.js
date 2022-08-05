7d541cf6166cc5d7220f67d44845ac9b6adf5ef9
chuckNorris506 <100477643+chuckNorris506@users.noreply.github.com>
2022-08-05 14:36:40 -0600

    HTTPS/TLS1.3
    -Se habilita servidor https
    -Se generan certificados tls

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const https = require("https");
+const fs = require("fs");
 const express = require('express')
 const app = express()
 require('dotenv').config()
 require('express-async-errors')
 
-const { authenticate,authenticateResetPassword } = require('./middleware/authenticate')
+const { authenticate, authenticateResetPassword } = require('./middleware/authenticate')
 const user = require('./routes/user')
 const professor = require('./routes/professor')
 const subject = require('./routes/subject')
@@ -25,6 +27,12 @@ app.use('/api/v1/campus', authenticate, campus)
 app.get('/reset-password/:token', authenticateResetPassword, user)
 app.use('*', notFound)
 
-app.listen(process.env.SERVER_PORT, async () => {
-    console.log(`Server is listening on port ${process.env.SERVER_PORT}`)
-})
+https.createServer({
+    // generar certificado self-signed
+    //https://adamtheautomator.com/https-nodejs/
+    key: fs.readFileSync('./certificate/key.pem'),
+    cert: fs.readFileSync('./certificate/cert.pem')
+}, app)
+    .listen(process.env.SERVER_PORT, async () => {
+        console.log(`Server is listening on port ${process.env.SERVER_PORT}`)
+    })
