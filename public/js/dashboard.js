1b9d958f70c69c0db851c6c201729be5b01a374c
chuckNorris506 <100477643+chuckNorris506@users.noreply.github.com>
2022-08-10 15:30:44 -0600

    Se modifica función datos gráficos

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -60,56 +60,109 @@ const getSubjects = async () => {
 }
 
 const getChartData = async () => {
-    getData('approved')
-    getData('failed')
-    getData('dropped')
-}
-
-const getData = async (option) => {
-
     const subject = document.getElementById('subject').value
     const campus = document.getElementById('campus').value
+    /**
+     * ACTUAL
+     */
+    let msg
+    let approved = []
+    let failed = []
+    let dropped = []
+    
+    await getData('approved', subject, campus)
+        .then(arr => { if (arr) { approved = arr } })
+        .catch((alert) => { msg = alert })
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'Authorization': sessionStorage.getItem('jwt')
-        }
-    }
+    await getData('failed', subject, campus)
+        .then(arr => { if (arr) { failed = arr } })
+        .catch(() => { })
 
-    fetch(`api/v1/course/${subject}/?campus=${campus}&option=${option}`, options)
-        .then(res => {
-            if (res.status === 200) {
+    await getData('dropped', subject, campus)
+        .then(arr => { if (arr) { dropped = arr } })
+        .catch(() => { })
 
-                res.json().then(res => {
-                    let mostRecent = Number(Object.values(res.shift()))
-                    let average = 0
-                    let averageCont = 0
+    if (approved.length > 1 || failed.length > 1 || dropped.length > 1) {
 
-                    res.forEach(record => {
-                        average += Number(Object.values(record))
-                        averageCont += 1
-                    })
-                    average /= averageCont
-                    const data = {
-                        mostRecent: mostRecent,
-                        average: average
-                    }
-                    
-                    createChart(data.mostRecent, data.average, option)
-                })
-            }
-            else if (res.status === 404) {
-                res.json().then(res => {
-                    if (option === 'dropped') {
-                        document.getElementById(`approved`).innerHTML = ""
-                        document.getElementById(`failed`).innerHTML = ""
-                        document.getElementById(`${option}`).innerHTML = res.msg
-                    }                    
-                })
-            }
+        let approvedMostRecent = approved.shift()
+        let failedMostRecent = failed.shift()
+        let droppedMostRecent = dropped.shift()
+        let totalMostRecent = approvedMostRecent + failedMostRecent + droppedMostRecent
+        alert(droppedMostRecent)
+        let pamr = approvedMostRecent * 100 / totalMostRecent
+        let pfmr = failedMostRecent * 100 / totalMostRecent
+        let pdmr = droppedMostRecent * 100 / totalMostRecent
+        
+        alert(pfmr)
+        let totalApprovedHistorical = 0
+        let totalFailedHistorical = 0
+        let totalDroppedHistorical = 0
+        let totalHistorical = 0
+
+        approved.forEach(element => {
+            totalApprovedHistorical += element
         })
-        .catch(err => alert(err))
+        failed.forEach(element => {
+            totalFailedHistorical += element
+        })
+        dropped.forEach(element => {
+            totalDroppedHistorical += element
+        })
+
+        totalHistorical = totalApprovedHistorical + totalFailedHistorical + totalDroppedHistorical
+
+        let pah = totalApprovedHistorical * 100 / totalHistorical
+        let pfh = totalFailedHistorical * 100 / totalHistorical
+        let pdh = totalDroppedHistorical * 100 / totalHistorical
+
+        createChart(pamr, pah, 'approved')
+        createChart(pfmr, pfh, 'failed')
+        createChart(pdmr, pdh, 'dropped')
+    }
+    else {
+        document.getElementById(`approved`).innerHTML = ""
+        document.getElementById(`failed`).innerHTML = ""
+        document.getElementById(`dropped`).innerHTML = msg
+    }
+}
+
+const getData = async (option, subject, campus) => {
+
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': sessionStorage.getItem('jwt')
+            }
+        }
+
+        fetch(`api/v1/course/${subject}/?campus=${campus}&option=${option}`, options)
+            .then(res => {
+                if (res.status === 200) {
+
+                    res.json().then(res => {
+                        let arr = []
+                        let count = 0
+
+                        res.forEach(record => {
+                            arr[count] = Number(Object.values(record))
+                            count++
+                        })
+                        resolve(arr)
+                    }
+                    )
+                }
+
+                else if (res.status === 404) {
+                    res.json().then(res => {
+                        reject(res.msg)
+                    })
+                }
+            })
+            .catch(err => alert(err))
+    })
+
+
 }
 
 const createChart = async (mostRecent, average, option) => {
@@ -138,14 +191,13 @@ const createChart = async (mostRecent, average, option) => {
                 break
         }
 
-        var options = { 'title': title, 'width': 400, 'height': 400, is3D: true, slices: { 1: { offset: 0.1 } } }
+        var options = { 'title': title, 'width': 400, 'height': 400, is3D: true, slices: { 1: { offset: 0.1 } }, pieSliceText: 'value' }
 
 
         var chart = new google.visualization.PieChart(document.getElementById(option))
         chart.draw(data, options)
     }
 }
-
 //https://developers.google.com/chart/interactive/docs/gallery/piechart
 //https://www.w3schools.com/howto/howto_google_charts.asp
 
